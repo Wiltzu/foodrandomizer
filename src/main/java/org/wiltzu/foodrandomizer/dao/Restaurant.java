f0081c5decc151e7ef7c87bb3196632c0c51dfe1
Ville Ahti <ville.ahti@nortal.com>
2012-08-06 15:15:08 +0300

    little refactoring (restaurant, SFRST)

--- src/main/java/org/wiltzu/foodrandomizer/dao/Restaurant.java
+++ src/main/java/org/wiltzu/foodrandomizer/dao/Restaurant.java
@@ -6,31 +6,33 @@ public class Restaurant {
 
 	String name;
 	List<Meal> meals;
-	
-	
+
 	public Restaurant(String name, List<Meal> meals) {
 		setName(name);
 		setMeals(meals);
 	}
-	
+
 	public String getName() {
 		return name;
 	}
+
 	private void setName(String name) {
 		this.name = name;
 	}
+
 	public List<Meal> getMeals() {
 		return meals;
 	}
+
 	private void setMeals(List<Meal> meals) {
 		this.meals = meals;
 	}
-	
-	public String getMealAsText (int index) {
+
+	public String getMealAsText(int index) {
 		try {
-			  Meal searchedMeal = meals.get(index);
-			  return searchedMeal.toString() + " (" + this.name + ")";
-		} catch(IndexOutOfBoundsException e) {
+			Meal searchedMeal = meals.get(index);
+			return searchedMeal.toString() + " (" + this.name + ")";
+		} catch (IndexOutOfBoundsException e) {
 			e.printStackTrace();
 		}
 		return "ERROR: meals list's index not found";
@@ -39,16 +41,15 @@ public class Restaurant {
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
-		
-		for(Meal meal : meals) {
+
+		for (Meal meal : meals) {
 			builder.append(meal);
-			if(meals.iterator().hasNext()) {
+
+			if (meals.iterator().hasNext())
 				builder.append("\n");
-			}
 		}
-		
+
 		return name + "\n" + "Meals: " + "\n" + builder;
 	}
-	
-	
+
 }
