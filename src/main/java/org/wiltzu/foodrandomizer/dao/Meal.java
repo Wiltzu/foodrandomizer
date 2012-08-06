ba7098a9c024162170f0cc8b730e272c1f4a1793
Ville Ahti <ville.ahti@nortal.com>
2012-08-06 15:40:29 +0300

    little refactoring

--- src/main/java/org/wiltzu/foodrandomizer/dao/Meal.java
+++ src/main/java/org/wiltzu/foodrandomizer/dao/Meal.java
@@ -1,24 +1,27 @@
 package org.wiltzu.foodrandomizer.dao;
 
 public class Meal {
-	
+
 	String name;
 	String price;
-	
+
 	public Meal(String name, String price) {
 		setName(name);
 		setPrice(price);
 	}
-	
+
 	public String getName() {
 		return name;
 	}
+
 	private void setName(String name) {
 		this.name = name;
 	}
+
 	public String getPrice() {
 		return price;
 	}
+
 	private void setPrice(String price) {
 		this.price = price;
 	}
